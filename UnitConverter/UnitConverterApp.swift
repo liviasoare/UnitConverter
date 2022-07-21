ce16070e7b4bfcfebcee5997ad48fdc832609cc7
Anca Iuga <liv13soare@gmail.com>
2022-07-22 01:11:29 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Livia Soare on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
