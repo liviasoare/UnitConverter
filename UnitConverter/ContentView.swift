54c37360c965d6311a9ca0199fe90ae74dffa628
Anca Iuga <liv13soare@gmail.com>
2022-07-22 01:54:39 +0300

    first commit

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputNumber = 0.0
+    @State private var inputUnit = "centimeters"
+    @State private var outputUnit = "meters"
+    
+    @FocusState private var numberIsFocused: Bool
+    
+    let units = ["picometers","nanometers","micrometers","millimeters", "centimeters", "decimeters", "meters", "dekameters", "hectometers", "kilometers"]
+    
+    var converted : Double = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter your value", value: $inputNumber, format: .number)
+                } header: {
+                    Text("Enter your value")
+                }
+                .keyboardType(.decimalPad)
+                .focused($numberIsFocused)
+                
+                Picker("Input Unit", selection: $inputUnit){
+                    ForEach(units, id: \.self){
+                        Text($0)
+                    }
+                }
+                
+                Picker("Output Unit", selection: $outputUnit){
+                    ForEach(units, id: \.self){
+                        Text($0)
+                    }
+                }
+                
+                Section{
+                    Text(inputNumber, format: .number)
+                    
+                } header: {
+                    Text("Result")
+                }
+                
+            }
+            .navigationTitle("Length Unit Converter")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    
+                    Button("Done"){
+                        numberIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
